a639c11f61272b4533a5af50a2899deb827e0f46
Bas Folkers <bfolkers07@hotmail.com>
2025-09-03 08:42:29 +0200

    Created portfiol page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: "full",
+        loadComponent: () => {
+            return import('./home/home.component').then((m) => m.HomeComponent)
+        },
+    },
+    {
+        path: 'market',
+        loadComponent: () => {
+            return import('./market/market.component').then((m) => m.MarketComponent)
+        },
+    },
+    {
+        path: 'portfolio',
+        loadComponent: () => {
+            return import('./portfolio/portfolio.component').then((m) => m.PortfolioComponent)
+        }
+    }
+];
